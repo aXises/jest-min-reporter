541a551486d97b8308ec69b9e2e2addc80b8e5f2
Xinyi Li <leletonyli.13944@yahoo.com.au>
2021-03-07 00:30:41 +1000

    Fixed config not finding correct jest environment

--- jest.config.ts
+++ jest.config.ts
@@ -1,10 +1,12 @@
 import type { Config } from "@jest/types";
+import path from "path";
 
 const config: Config.InitialOptions = {
     preset: "ts-jest",
     testEnvironment: "node",
     verbose: true,
-    reporters: [["./dist", { colours: true, diffs: true }]],
+    reporters: [[path.join(__dirname, "dist"), { colours: true, diffs: false }]],
+    testMatch: [path.join(__dirname, "test", "**", "*")],
 };
 
 export default config;
